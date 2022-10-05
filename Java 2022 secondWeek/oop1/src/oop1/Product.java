c3f8d09c293a30187caa24e718ff9766a33091f9
Yusuf <gundogdu.yusuf@hotmail.com>
2022-10-05 19:46:24 +0300

    Java 2022 secondWeek
    Java 2022 secondWeek Youtube Live Course

---
+++ Java 2022 secondWeek/oop1/src/oop1/Product.java
@@ -0,0 +1,53 @@
+package oop1;
+
+//Burada ürünü ve diğer ürünleri tanımlayabileceğimiz ortamı hazırlıyoruz
+//PascalCase bir isimlendirme şeklidir. Kelimelerin ilk harfleri büyük olur
+public class Product {
+	private String name; // Private sadece o class ın içinde kullanılabilir demek
+	// Field lar camelCase olarak yazılır:unitPrice,unitInStock gibi
+	private double unitPrice;
+	private double discount;
+	private String imageUrl;
+	private int unitInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitInStock() {
+		return unitInStock;
+	}
+
+	public void setUnitInStock(int unitInStock) {
+		this.unitInStock = unitInStock;
+	}
+
+}
